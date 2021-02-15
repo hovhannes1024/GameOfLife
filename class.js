59f0e6dad668b919014d9e9ae31196baea30462b
hovhannes1024 <hovhannes.m1.y@tumo.org>
2021-02-15 17:25:46 +0400

    parents

--- class.js
+++ class.js
@@ -310,34 +310,22 @@ class Water extends Parent{
         }
     }
 }
-class Lightning{
+class Lightning extends Parent{
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
+        super(x, y);
         this.multiply = 0;
-        this.directions = [];
     }
     getDirections() {
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x,     this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1,     this.y],
-            [this.x + 1,     this.y],
-            [this.x - 1, this.y + 1],
-            [this.x,     this.y + 1],
-            [this.x + 1, this.y + 1],
-            [this.x,         this.y]
-    ];
-    var found = [];
-    for (var i in this.directions) {
-        var x = this.directions[i][0];
-        var y = this.directions[i][1];
-        if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-            found.push(this.directions[i]);
+        this.newDirections();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                found.push(this.directions[i]);
+            }
         }
-    }
-    return found;
+        return found;
     }
     move() {
         let cord = this.getDirections();
