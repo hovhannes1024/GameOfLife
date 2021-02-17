fffc6e06ee27d7ab155fa836d2f1a728ca5fb055
hovhannes1024 <hovhannes.m1.y@tumo.org>
2021-02-17 11:43:25 +0400

    modules_1

---
+++ classes/Parent.js
@@ -0,0 +1,19 @@
+module.exports = class Parent {
+    constructor(x, y,){
+        this.x = x;
+        this.y = y;
+        this.multiply = 0;
+    }
+    newDirections() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+}
